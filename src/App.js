a2c5a046728c7cdaca90b22fb4e098b2e0cd87ea
Pamir Perdeci <pamirperdeci@gmail.com>
2023-09-28 10:37:55 -0400

    animated button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
+const moveRightAndLeft = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(100px); /* Adjust the distance for your desired effect */
+  }
+  100% {
+    transform: translateX(0);
+  }
+`;
 
 const StyledButton = styled.button`
-  background-color: lightblue;
+  background-color: red;
   color: black;
   font-size: 50px;
   padding: 10px 20px;
   border: none;
   cursor: pointer;
+  animation: ${moveRightAndLeft} 2s linear infinite;
+  
 `;
+
+
+const StyledDiv = styled.div`
+  background-color: green;
+  color: black;
+  font-size: 50px;
+  padding: 10px 20px;
+  border: 1px solid white;
+  cursor: pointer;
+  height: 100%;
+
+`;
+
+
+
 
 function App() {
   return (
-    <div className="App">
+    <StyledDiv className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -28,9 +56,9 @@ function App() {
         >
           Learn React
         </a>
-        <StyledButton>Styled Button</StyledButton>
+        <StyledButton>Don't Press </StyledButton>
       </header>
-    </div>
+    </StyledDiv>
   );
 }
 
